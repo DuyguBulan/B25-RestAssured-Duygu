26fb442b43a79e761f7ab355b48ed160a3d4f891
duygubulan <duygum_ay@hotmail.com>
2022-04-19 15:07:04 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -0,0 +1,28 @@
+package com.cydeo.day2;
+
+import org.junit.jupiter.api.Test;
+
+public class SpartanGetRequests {
+
+    String url = "http://3.82.205.143:8000";
+
+    /*
+
+    Given Accept type is aplication/json
+    Then status code must be 200
+    And response content type must be application/json
+     */
+
+    @Test
+    public void test(){
+
+
+
+
+
+
+
+    }    
+
+
+}
